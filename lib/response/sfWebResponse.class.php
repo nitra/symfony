2b37e30a766379d3f28dbfa0e7f8f35645244260
a.onatskyy <onatskyy@nitralabs.com>
2013-11-14 13:06:15 +0200

    Изменены функции pregtr и normalizeHeaderName для корректной работы приложений на PHP 5.5

--- lib/response/sfWebResponse.class.php
+++ lib/response/sfWebResponse.class.php
@@ -404,9 +404,14 @@ class sfWebResponse extends sfResponse
    *
    * @return string Normalized header
    */
-  protected function normalizeHeaderName($name)
-  {
-    return preg_replace('/\-(.)/e', "'-'.strtoupper('\\1')", strtr(ucfirst(strtolower($name)), '_', '-'));
+ protected function normalizeHeaderName($name)
+ {
+        // return preg_replace('/\-(.)/e', "'-'.strtoupper('\\1')", strtr(ucfirst(strtolower($name)), '_', '-'));
+        return preg_replace_callback(
+                        '/\-(.)/', function ($matches) {
+                            return '-' . strtoupper($matches[1]);
+                        }, strtr(ucfirst(strtolower($name)), '_', '-')
+        );
   }
 
   /**
